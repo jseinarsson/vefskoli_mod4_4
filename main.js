1a39b347e2fac71ab645ae00fbb06f02bde84b19
Jakob Sturla <jseinarsson@gmail.com>
2020-11-07 21:32:23 +0000

    f this...

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+// let countries = [];
+
+// async function loadCountries(region) {
+//     const res = await fetch(`https://restcountries.eu/rest/v2/region/${region}`);
+
+//     if (!res.ok) {
+//         console.log('Error');
+//     } else {
+//         console.log(`Ok, got ${region}`);
+//         return await res.json();
+//     }
+// }
+
+// regionBtns.forEach(function (btn) {
+//     btn.addEventListener('click', async () => {
+//         try {
+//             countries = await loadCountries(btn.id);
+//         } catch (err) {
+//             error.innerHTML = (`<h1 class="alert">Something went wrong... please try again.</h3>`);
+//         }
+
+//         return countries;
+//     });
+// });
+
+// console.log(countries);
+
 // // On dom load, the loadcountries function is run and tries to assign the JSON string to the countries object, else it console logs the error and changes the DOM contents
 // document.addEventListener("DOMContentLoaded", async () => {
 //     try {
@@ -10,53 +37,10 @@
 //     return countries;
 // });
 
-// Load API with info on European countries
-// async function loadCountries() {
-//     const res = await fetch('https://restcountries.eu/rest/v2/region/europe');
+const regionBtns = document.querySelectorAll(".regionBtn");
+const error = document.querySelector(".error");
 
-//     if (!res.ok) {
-//         throw new Error('Network response error');
-//     } else {
-//         return await res.json();
-//     }
-// }
-
-// const countries = document.addEventListener("DOMContentLoaded", async () => {
-//     try {
-//         await loadCountries();
-//     } catch (err) {
-//         body.innerHTML = (`<h1 class="alert">Something went wrong... please try refreshing the page.</h3>`);
-//     }
-// });
-
-// console.log(countries);
-
-
-// const regionBtns = document.querySelectorAll('.regionBtn');
-
-// async function loadCountries(region) {
-//     const res = await fetch(`https://restcountries.eu/rest/v2/region/${region}`);
-
-//     if (!res.ok) {
-//         throw new Error('Network response error');
-//     } else {
-//         return await res.json();
-//     }
-// }
-
-// regionBtns.forEach(function (btn) {
-//     btn.addEventListener('click', async () => {
-//         try {
-//             return await loadCountries(btn.id);
-//         } catch (err) {
-//             error.innerHTML = (`<h1 class="alert">Something went wrong... please try refreshing the page.</h3>`);
-//         }
-//     });
-// });
-
-const error = document.querySelector('.error');
-
-function loadCountries(region) {
+function getCountries(region) {
     fetch(`https://restcountries.eu/rest/v2/region/${region}`)
         .then(res => {
             if (!res.ok) {
@@ -65,26 +49,13 @@ function loadCountries(region) {
                 return res.json();
             }
         })
-        .then(data => {return data})
+        .then(data => console.log(data))
         .catch(err => {
-            console.error(err)
-            error.innerHTML = (`<h1 class="alert">Something went wrong... please try again.</h3>`)
+            console.log(err);
+            error.innerHTML = (`<h3>Something went wrong... please try again</h3>`)
         });
 }
 
-// const europe = loadCountries('europe');
+const europe = getCountries('europe');
 
-console.log(loadCountries('europe'));
-
-// async function loadCountries(region) {
-//         const res = await fetch(`https://restcountries.eu/rest/v2/region/${region}`);
-
-//     if (!res.ok) {
-//         throw new Error('Network response error');
-//     } else {
-//         return await res.json();
-//     }
-// }
-
-// const europe = loadCountries('europe');
-// console.log(europe);
+console.log(europe);
